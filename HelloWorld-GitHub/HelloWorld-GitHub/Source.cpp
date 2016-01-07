b194e9d37649aa40cf18435cff8740feebabc837
pstems <pstemler@gmail.com>
2016-01-07 11:13:15 -0800

    Added addition function.

--- HelloWorld-GitHub/HelloWorld-GitHub/Source.cpp
+++ HelloWorld-GitHub/HelloWorld-GitHub/Source.cpp
@@ -2,9 +2,19 @@
 
 using namespace std;
 
+int addition1(int a, int b);
+
 int main()
 {
 	cout << "Hello World!" << endl;
 	cout << "My first program." << endl;
+
+	cout << addition1(4, 8) << endl;
+
 	return 0;
+}
+
+int addition1(int a, int b)
+{
+	return a + b;
 }
